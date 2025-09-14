554898ffea4cf4139eb2e7dc0dcd1c86082b4896
vishalk4 <vishalkottu87@gmail.com>
2025-09-14 12:18:54 +0530

    identifying armstrong number

---
+++ armstrong_number.py
@@ -0,0 +1,13 @@
+"""An Armstrong number with n digits is a number such that if each of its digits is
+raised to the power of n and added together, the sum equals the number itself"""
+def armstrong_number(n):
+    sum = 0
+    store = n
+    while n > 0:
+        sum = sum + (n % 10) **len(str(store))
+        n = n // 10
+    if sum == store:
+        print("The number is an Armstrong number")
+    else:
+        print("The number is not an Armstrong number")
+armstrong_number(407)
