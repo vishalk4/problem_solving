ccdf56ef19674a9f6daec95dabf026d1b5b23b7a
vishalk4 <vishalkottu87@gmail.com>
2025-09-25 22:56:05 +0530

    move all the zeroes in list to end

---
+++ move_zeroes_to_end.py
@@ -0,0 +1,8 @@
+def moveZeroes(nums):
+    j = 0
+    for i in range(len(nums)):
+        if nums[i] != 0:
+            nums[j],nums[i] = nums[i],nums[j]
+            j += 1
+    return nums
+print(moveZeroes([0,2,3,0,4,0,5,0,4]))
