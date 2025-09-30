a3b7a6b96ae88eb03dd03902b64c9f8796e36ca8
vishalk4 <vishalkottu87@gmail.com>
2025-09-30 18:29:29 +0530

    search for an element in a list by binary search

--- binary_search.py
+++ binary_search.py
@@ -1,12 +1,13 @@
-def binary_search(arr, target):
-    left = 0
-    right = len(arr) - 1
-    while left <= right:
-        mid = (left + right) // 2
-        if arr[mid] == target:
+def binary_search(nums, target):
+    start = 0
+    end = len(nums)-1
+    while start <= end:
+        mid = (start+end)//2
+        if nums[mid] == target:
             return mid
-        elif arr[mid] < target:
-            left = mid + 1
+        elif nums[mid] < target:
+            start = mid+1
         else:
-            right = mid - 1
+            end = mid-1
     return -1
+print(binary_search([1,2,3,5,6,7,8,9], 0))
