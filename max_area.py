c465e7d32be2049d0ece1ac230c60d7afece2af1
KOTTU VISHAL <vishalkottu87@gmail.com>
2026-06-13 00:10:52 +0530

    Implement maxArea method for container problem

---
+++ max_area.py
@@ -0,0 +1,19 @@
+class Solution:
+    def maxArea(self, height):
+        left = 0
+        right = len(height) - 1
+        mw = 0 # variable to store maximum water
+        while left < right: # loop until pointers meet
+            width = right - left # width between the two lines
+            # height is the length of shorter line
+            ch = min(height[left], height[right])
+            # Calculate current area
+            cw = width * ch
+            # Update maximum water if current is larger
+            mw = max(mw, cw)
+            # Move the pointer with smaller height
+            if height[left] < height[right]:
+                left += 1
+            else:
+                right -= 1
+        return mw
